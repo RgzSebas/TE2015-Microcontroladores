c19309ae1d483a60179e909c3b95af036341d9b4
Matias Vazquez <matvaz@dtu.dk>
2022-09-02 15:38:43 +0200

    corrected instruction 1

--- mplab/HW2.X/hw2_main.s
+++ mplab/HW2.X/hw2_main.s
@@ -5,6 +5,7 @@ PROCESSOR   18F57Q43
 ; PAGE SECTION FOR VARIABLES 
 PSECT UDATA_ACS
     VAR:  DS 1
+    AUX:  DS 1
 
 PSECT	resetVec, class=CODE, reloc=2
 
@@ -32,15 +33,15 @@ MAIN:
     MOVLB   0x04	;Set Bank 4
     CLRF    TRISA,1	;Set PORT A as output (BSR)
 
-    ; 1) Use the current value on WREG and decrement it. Keep the result on WREG.
+; 1) Assign literal 01101101B to Port A through WREG
     MOVLW   01101101B	;Move 0X6D to WREG
     MOVWF   LATA,1	;Move WREG to LATA (BSR)
 
-    ; 2) Invert the value on WREG and assign in to a variable called \texttt{VAR}.
+; 2) Invert the value on WREG and assign in to a variable called VAR.
     COMF    WREG,0,0	    ;Complement WREG and save in WREG (Access Bank)
     MOVWF   BANKMASK(VAR),0 ;Move WREG to masked VAR address (Access Bank)
 
-    ; 3) Use the current value on WREG and add literal 0x02. Keep the result on WREG.
+; 3) Use the current value on WREG and add literal 0x02. Keep the result on WREG.
     ADDLW   0x02	;Add 0x02 to WREG
 
 LOOP:
